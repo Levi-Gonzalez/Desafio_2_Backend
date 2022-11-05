a775a2bdda0b979d03683feab60d18ddd616d7d9
Levi-Gonzalez <levigonzalez.t@gmail.com>
2022-11-05 05:35:21 -0300

    pros

--- desafio2.js
+++ desafio2.js
@@ -5,31 +5,67 @@ class Contenedor {
         this.path = path
     }
 
-    save(){
-
+    async save(objeto){
+        try{
+            const leer = await fs.readFile(this.path, "utf-8");
+            const data= JSON.parse(leer)
+            let id;
+            data.length == 0
+                ? (id = 1)
+                : (id = data [data.length - 1].id + 1)
+            const newProducto = {...objeto,id}
+            data.push(newProducto)
+             await fs.writeFile(this.path, JSON.stringify(data, null, 2), "utf-8")
+            return newProducto.id
+        }catch (e){
+            console.log(e);
+        }
     }
-    getById(){
-
+    async getById(id){
+        try {
+            const leer = await fs.readFile(this.path, "utf-8");
+            const data = JSON.parse(leer)
+            const obj = data.find(obj =>obj.id == id)
+            if(!obj){
+                return null
+            }
+            return obj
+        } catch (e) {
+            console.log(e);
+        }
     }
     async getAll(){
-        const leer = await fs.readFile(this.path, "utf-8") 
+        const leer = await fs.readFile(this.path, "utf-8");
         return JSON.parse(leer)
     }
     deleteById(){
 
     }
-    deteeAll(){
-
+    async deleteAll(){
+        try {
+            await fs.writeFile(this.path , JSON.stringify([], null , 2), "utf-8")
+        } catch (e){
+            console.log(e);
+        }
     }
 }
-module.exports = Contenedeor
+module.exports = Contenedor
 /*
 Donde va una promesa automaticamente debo definir "async" al método donde voy a trabajar. Todo lo que tenga demora es asincrono!
 tiene que leer un archivo que no está en el es script, eso requerira tiempo, por lo que es asincrono.
 
  # Si esta en el mismo script hacemos una función sincrona común.
- 
+ METODOS:
+ / GetAll: lee mis archivos productos del JSON
 linea: 15 col: 28, como es un archivo Json se parsea entonces returnamos: linea 16
 
 /fs linea 1 devuelve un objeto que es una promesa. (arregla el error de los 3 puntos que aparece en await)
- */
+ 
+en la terminal: node index.js = no me tiene que devolver nada
+
+
+try y catch nos ayuda a precindir el error que nos puede surgir siendro mas preciso en mostrar el problema y NODE nos dice el error.
+
+# Save(): es el más díficil, este perteence al de crear. Haciendo bien este método podemos sacar la lógica de todos los demás.
+siempre va a estar acompañado de un try & catch
+*/  
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const Conteiner = requiere("./index.js")
+const Conteiner = require ("./desafio2.js")
 const productos = new Conteiner ("./productos.json")
 
 async function misProductos (){
+
+    const objeto5 ={
+        nombre: "Micrófono",
+        precio: 1000,
+        thumbnail: "url"
+    }
+    const objeto4 ={
+        nombre: "Audífonos",
+        precio: 1500,
+        thumbnail: "url"
+    }
     const objeto3 = {
         nombre: "Teclado",
         precio: 2000,
@@ -17,8 +28,20 @@ async function misProductos (){
         precio: 1000,
         thumbnail: "url"
     }
-    await productos.getAll().then(a=>console.log(a))
-
-
+    await productos.getById(1).then(id => console.log(id))
+    await productos.save(objeto1)
+    await productos.save(objeto2)
+    await productos.save(objeto3)
+    await productos.save(objeto4)
+    await productos.save(objeto5)
 }
 misProductos()
+/*
+ await nos dice que lo que viene es una promesa "productos.getAll()" no es una funcion comun
+ entonces usamos el #then para terminar de consumir los datos que trae el retorno
+
+"await productos.getById(2)" esto es una promesa entonces para buscar el ID debo ponerlo de esta manera:
+"await productos.getById(2).then(id → console.log(id))" 
+
+
+*/
